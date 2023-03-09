fe18cebabf1040e4de0ef1bd693d7704fde0c374
Aindrew <abarariu@EN1510035>
2023-03-09 11:49:42 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int BoxA,BoxB,BoxC,BoxD,BoxE,BoxF,BoxG;
+BoxA = 34 ;
+BoxB = 4 ;
+BoxC = BoxA + BoxB;
+BoxD = BoxA - BoxB;
+BoxE = BoxA * BoxB;
+BoxF = BoxA / BoxB;
+BoxG = BoxA % BoxB;
+
+Console.WriteLine("Suma dintre BoxA si BoxB este = " + BoxC);
+Console.WriteLine("Scaderea dintre BoxA si BoxB este = " + BoxD);
+Console.WriteLine("Inmultirea dintre BoxA si BoxB este = " + BoxE);
+Console.WriteLine("Impartirea dintre BoxA si BoxB este = " + BoxF);
+Console.WriteLine("Restul dintre BoxA si BoxB este = " + BoxG);
